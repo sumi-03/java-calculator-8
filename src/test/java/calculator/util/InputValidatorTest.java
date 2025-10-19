6f8b1cf3fb3e416bc69ab0d09914f825c930a468
sumi <lsmkoo03@naver.com>
2025-10-19 19:15:39 +0900

    test: 허용되지 않은 문자 검증 테스트 작성 및 given/when/then 주석 적용

--- src/test/java/calculator/util/InputValidatorTest.java
+++ src/test/java/calculator/util/InputValidatorTest.java
@@ -4,14 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import java.util.regex.Matcher;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InputValidatorTest {
 
     @Test
     void 커스텀구분자_형식이_올바르면_Matcher를_반환한다() {
+        // given
+        String input = "//;\n1;2;3";
 
-        Matcher matcher = InputValidator.validateCustomDelimiterFormat("//;\n1;2;3");
+        // when
+        Matcher matcher = InputValidator.validateCustomDelimiterFormat(input);
+
+        // then
         assertNotNull(matcher);
         assertEquals(";", matcher.group(1)); // 구분자
         assertEquals("1;2;3", matcher.group(2)); // 본문
@@ -19,7 +26,34 @@ public class InputValidatorTest {
 
     @Test
     void 커스텀구분자_형식이_잘못되면_예외를_발생시킨다() {
+        // given
+        String input = "//;\r1;2";
+
+        // when & then
         assertThrows(IllegalArgumentException.class,
-                () -> InputValidator.validateCustomDelimiterFormat("//;\r1;2"));
+                () -> InputValidator.validateCustomDelimiterFormat(input));
+    }
+
+    @Test
+    void 허용된_문자만_포함된_경우_통과한다() {
+        // given
+        String input = "//;\n1;2;3";
+        String delimiter = ",|:|;";
+
+        // when & then
+        assertThatCode(() -> InputValidator.validateAllowedCharacters(input, delimiter))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 허용되지_않은_문자가_포함된_경우_예외를_발생시킨다() {
+        // given
+        String input = "//;\n1;2;3a";
+        String delimiter = ",|:|;";
+
+        // when & then
+        assertThatThrownBy(() -> InputValidator.validateAllowedCharacters(input, delimiter))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("허용되지 않은 문자");
     }
 }
